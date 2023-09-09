dc41a77fdccdd9679bc60783a3e9ca062c48a0d0
reliek21 <keilerguardo@gmail.com>
2023-09-09 09:48:07 -0500

    feat: structure added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
+
+const dateTime = new Date();
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  publicDir: 'public/',
+  site: 'https://www.agenciatopty.com',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: '1.0',
+			lastmod: `${dateTime.getFullYear()}-${dateTime.getMonth() }-${dateTime.getDay()}`,
+    }),
+  ],
+  output: 'server',
+  adapter: vercel({
+    imageService: true,
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+  }),
+});
