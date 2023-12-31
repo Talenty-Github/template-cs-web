1b5448e47a00c744a1bfc4d6eab6c77836d2c944
Talenty Tech <108200038+talenty-tecnologia@users.noreply.github.com>
2023-12-30 22:55:38 -0500

    fix: import added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/serverless';
+import { templateData } from './template.config';
 
 const dateTime = new Date();
 
